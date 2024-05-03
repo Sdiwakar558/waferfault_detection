dd984a525fc723ad4438be51c2c1c01e71323ad9
Sdiwakar558 <sdiwakar558@gmail.com>
2024-05-03 10:33:20 +0530

    first commit

--- Upload_CSVTOS3/mearge_csv_for_s3.py
+++ Upload_CSVTOS3/mearge_csv_for_s3.py
@@ -8,9 +8,14 @@ class upload_csv_to_s3:
         self.S3_fileOBJ = "/waferfaultdatacsv/ML_DATASET/wafer_prediction.csv"
 
     def mearge_all_csv(self):
+        final_dataframe = pd.DataFrame()
         for file in os.listdir(self.good_file_path):
-            pd.read_csv(file)
+            df_file = pd.read_csv(file)
+            final_dataframe = pd.merge(final_dataframe,df_file)
+        final_dataframe.to_csv("")
     def upload_csv_s3(self):
         s3_client = boto3.client('s3')
         try:
             s3_client.upload_file()
+        except:
+            pass
